0862abaf9358225e2a0aaa081b9b5edd17bf8ac2
Tomas Cejka <tomas.cejka@strv.com>
2020-03-21 22:22:56 +0100

    [hotfix] cached message to set own id correctly

--- ChatCore/Models/CachedMessage.swift
+++ ChatCore/Models/CachedMessage.swift
@@ -13,10 +13,11 @@ struct CachedMessage<T: MessageSpecifying & Cachable>: Codable {
 
     let content: T
     let conversationId: ObjectIdentifier
-    private let id: ObjectIdentifier = UUID().uuidString
+    private let id: ObjectIdentifier
     private(set) var state: CachedMessageState
 
     init(content: T, conversationId: ObjectIdentifier, state: CachedMessageState) {
+        id = UUID().uuidString
         self.content = content
         self.conversationId = conversationId
         self.state = state
