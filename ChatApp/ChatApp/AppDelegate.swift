d2b5d57f31a8ec08952c6bb570438068f4177a0f
Daniel Pecher <dan.pecher@gmail.com>
2020-03-28 14:23:36 +0100

    Use AppDelegate as sample ChatUIDelegate

--- ChatApp/ChatApp/AppDelegate.swift
+++ ChatApp/ChatApp/AppDelegate.swift
@@ -12,16 +12,8 @@ import Chat
 // swiftlint:disable implicitly_unwrapped_optional
 var chat: Chat!
 
-class ChatDelegate: Chat.UIDelegate {
-    public func emptyConversationsListAction() {
-        print("didTapTakeAQuiz")
-    }
-}
-
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-    
-    private let chatDelegate = ChatDelegate()
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
@@ -45,7 +37,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         )
         
         chat = Chat(networkConfig: networkConfig, uiConfig: uiConfig)
-        chat.uiDelegate = chatDelegate
+        chat.uiDelegate = self
         
         setupBackgroundFetch()
 
@@ -86,5 +78,11 @@ extension AppDelegate {
             return
         }
         completionHandler(.noData)
+    }
+}
+
+extension AppDelegate: Chat.UIDelegate {
+    func emptyConversationsListAction() {
+        print("Take a Quiz button tapped!")
     }
 }
